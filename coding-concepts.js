ee423f2b09d6c2787f186df213fa0f001b8d374d
triciasykes <tlsykes98@gmail.com>
2024-02-23 11:07:31 -0800

    review completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -11,26 +11,26 @@
 const cohort = "Alpha 2024"
 // console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Alpha" "2024"
+// b) Verify and explain: ["Alpha", "2024"] - .split() splits string into substrings in an array
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
-  `Hello, ${name}!`
+  ;`Hello, ${name}!`
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Hello LEARN Student"
+// b) Verify and explain: functions need return statements; this is undefined
 
 // --------------------3) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: array is filtered - if number is not even (doesn't divide evenly by 0)
 
 // --------------------4) What will this log?
 
@@ -38,12 +38,12 @@ const myCodingSkills = {
   languages: ["JavaScript", "Ruby"],
   frameworks: ["React", "Ruby on Rails"],
   databases: "PostgreSQL",
-  versionControl: "GitHub"
+  versionControl: "GitHub",
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: calls on object uses dot notation to pull out languages then bracket notation to get value at 0 index
 
 // --------------------5) What will this log?
 
@@ -57,5 +57,5 @@ class Learn {
 const learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
+// a) Your answer: Learn { student: "George", cohort: "Alpha", year: 2024}
 // b) Verify and explain:
